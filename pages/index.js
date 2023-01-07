a71ab8f95b73a6ba970f014b0fa533ff92b4bd7d
Rodrigo Santos <rodrigomadureira8@gmail.com>
2023-01-06 23:51:06 -0300

    final css

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
             <div id='azul'>
               <img src="qrcode 1.svg" alt="qrcode"/>
             </div>  
-            <p id='title'>Improve your front-end skills by bulding projectss</p>
-            <p id='p'>Scan the QR code to visit Frontend Mentor and Take your conding skills to the next level</p>
+            <p id='title'>Improve your front-end skills by bulding projects</p>
+            <p id='p' className='text-info recuo'> Scan the QR code to visit Frontend Mentor and Take your conding skills to the next level</p>
           </div>
-        </main>
+        </main> 
 
     </div>
   )
