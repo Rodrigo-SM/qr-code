117940594173c71d8f0bc1aec728166b6e330c70
Rodrigo Santos <rodrigomadureira8@gmail.com>
2023-01-09 17:20:04 -0300

    commit final

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import styled from 'styled-components'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,66 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 
 
+export const CardWhite = styled.div`
+background-color: #fff;
+  position: absolute;
+  top: 15%;
+  left: 35%;
+  width: 22em;
+  height: 30em;
+  padding-left: 1.5em;
+  padding-top: 1em;
+  padding-right: 1.5em;
+  box-shadow: 3px 3px 3px 4px #cdf;
 
+  h1{position: absolute;
+  margin-top: 10em;
+  margin-left: 1.5em;
+  margin-right: 1.5em;
+  color: #2e374b;
+  font-size: 1.6em;
+  font-weight: 600;}
+
+  p{
+    position: absolute;
+  margin-top: 15em;
+  color: #b6b9c2;
+  font-size: 1.4em;
+  margin-left: 1.8em;
+  margin-right: 2em;
+  text-indent: 1em;
+  }
+
+`;
+
+export const CardBlue = styled.div`
+background-color: #2c7dfa;
+  box-shadow: 5px 5px 3px #2c7ada;
+  position: absolute;
+  display: flex;
+  height: 15em;
+  width: 22em;
+ 
+  img{
+  width: 11em;
+  height: 11em;
+  margin-left: 5.6em;
+  margin-top: 2em;
+}
+`;
 
 export default function Home() {
   return (
     <div>
       
-        <main>
-          <div id='branco'>
-            <div id='azul'>
-              <img src="qrcode 1.svg" alt="qrcode"/>
-            </div>  
-            <p id='title'>Improve your front-end skills by bulding projects</p>
-            <p id='p' className='text-info recuo'> Scan the QR code to visit Frontend Mentor and Take your conding skills to the next level</p>
-          </div>
-        </main> 
-
+       
+          <CardWhite>     
+            <CardBlue><img src="qrcode.svg" alt="code"/></CardBlue>   
+            <h1>Improve your front-end skills by bulding projects</h1>
+            <p>Scan the QR code to visit Frontend Mentor and Take your conding skills to the next level</p>
+          </CardWhite>
+          
     </div>
   )
 }
