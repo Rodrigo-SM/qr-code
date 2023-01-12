6721a547be95b873c48ff644bc87fc17d24934e7
Rodrigo <rodrigomadureira8@gmail.com>
2023-01-12 20:38:10 -0300

    final 1.1

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,32 @@
 import Head from 'next/head'
 import styled from 'styled-components'
-
+import axios from 'axios'
+import {useState} from 'react'
 
 export const CardWhite = styled.div`
 background-color: #fff;
-  position: absolute;
-  top: 15%;
-  left: 35%;
-  width: 22em;
-  height: 30em;
-  padding-left: 1.5em;
-  padding-top: 1em;
-  padding-right: 1.5em;
-  box-shadow: 3px 3px 3px 4px #cdf;
 
-  h1{position: absolute;
-  margin-top: 10em;
-  margin-left: 1.5em;
-  margin-right: 1.5em;
+  width: 300px;
+
+ padding: 30px 30px;
+  
+ box-shadow: 0px 0px 5px 2px #00040845;
+
+  h1{
   color: #2e374b;
   font-size: 1.6em;
-  font-weight: 600;}
+  font-weight: 600;
+  margin: 10px 0px 0px 5px;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
   p{
-    position: absolute;
-  margin-top: 15em;
+   margin: 0px;
+ text-align: left;
+ font-family: Arial, Helvetica, sans-serif;
   color: #b6b9c2;
   font-size: 1.4em;
-  margin-left: 1.8em;
-  margin-right: 2em;
-  text-indent: 1em;
+  margin: 10px 0px 0px 5px;
   }
 
 `;
@@ -37,30 +34,54 @@ background-color: #fff;
 export const CardBlue = styled.div`
 background-color: #2c7dfa;
   box-shadow: 5px 5px 3px #2c7ada;
-  position: absolute;
   display: flex;
   height: 15em;
-  width: 22em;
- 
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  
+
   img{
-  width: 11em;
-  height: 11em;
-  margin-left: 5.6em;
-  margin-top: 2em;
+    width: 100%;
+    height: 100%;
+ 
 }
 `;
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+`
+
 export default function Home() {
+  const [pokemonData, setPokemonData] = useState([])
+
+  const getDataPokemon = async () => {
+    const data = await axios.get('https://pokeapi.co/api/v2/pokemon')
+
+    setPokemonData(data.data.results)
+  }
+
   return (
-    <div>
-      
-       
+    <Container>
           <CardWhite>     
             <CardBlue><img src="qrcode.svg" alt="code"/></CardBlue>   
             <h1>Improve your front-end skills by bulding projects</h1>
             <p>Scan the QR code to visit Frontend Mentor and Take your conding skills to the next level</p>
           </CardWhite>
-          
-    </div>
+
+          {pokemonData.length === 0 ? null : <>
+          {pokemonData.map(value => (
+            <div style={{background: 'blue'}} >
+          {value.name}
+            </div>
+          ))}
+          </>}
+          <button onClick={()=> getDataPokemon()}>adasdasdsad</button>
+
+    </Container>
   )
 }
